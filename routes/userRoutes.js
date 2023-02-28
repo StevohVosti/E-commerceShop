95bb327f3b55987efb005b5d232be16225ddc008
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-02-28 15:54:10 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,9 +21,9 @@ router.route("/register").post(registerUser);
 
 router.route("/login").get(loginUser);
 
-// router.route("/password/forgot").post(forgotPassword);
+router.route("/password/forgot").post(forgotPassword);
 
-// router.route("/password/reset/:token").put(resetPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
 
@@ -37,10 +37,10 @@ router
   .route("/admin/users")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
 
-// router
-//   .route("/admin/user/:id")
-//   .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
-//   .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
-//   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
