480be0eab1176fb9f7da90d8b0c2a203eaf5e123
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-03-06 13:10:30 +0300

    Add files via upload

---
+++ frontend/src/actions/productAction.js
@@ -0,0 +1,9 @@
+// Get products
+export const getProduct = () => async(dispatch) => {
+
+};
+
+// Clearing Errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({ type: "ClearErrors" });
+};
