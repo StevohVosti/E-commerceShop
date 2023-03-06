6e0994a8e3758fd2f59f911b2e8be776c9dc93f6
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-03-06 13:09:15 +0300

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./reducers/userReducers";
+import { productsReducer } from "./reducers/productReducers";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    products: productsReducer,
   },
 });
 
