b15191f47e54b56b5784233fced46309fdd9dc9d
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-03-14 19:01:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const path = require("path");
+// const path = require("path");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -19,14 +19,20 @@ app.use(fileUpload());
 
 // Route Imports
 const product = require("./routes/productRoute");
-const user = require("./routes/userRoute");
-const order = require("./routes/orderRoute");
-const payment = require("./routes/paymentRoute");
+const user = require("./routes/productRoute");
+// const order = require("./routes/orderRoute");
+// const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
-app.use("/api/v1", order);
-app.use("/api/v1", payment);
+// app.use("/api/v1", order);
+// app.use("/api/v1", payment);
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 // Middleware for Errors
 app.use(errorMiddleware);
