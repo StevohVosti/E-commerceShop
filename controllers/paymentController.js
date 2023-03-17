0b4b00041a62d2407a8d63bb10e11ddd5fc59a87
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-03-17 12:51:10 +0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,20 +2,32 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.processPayment = catchAsyncErrors(async (req, res, next) => {
-  const myPayment = await stripe.paymentIntents.create({
-    amount: req.body.amount,
-    currency: "inr",
-    metadata: {
-      company: "Ecommerce",
-    },
-  });
+exports.processPayment = async (req, res, next) => {
+  try {
+    const myPayment = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "inr",
+      metadata: {
+        company: "Ecommerce",
+      },
+    });
 
-  res
-    .status(200)
-    .json({ success: true, client_secret: myPayment.client_secret });
-});
+    res
+      .status(200)
+      .json({ success: true, client_secret: myPayment.client_secret });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
-exports.sendStripeApiKey = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
-});
+exports.sendStripeApiKey = async (req, res, next) => {
+  try {
+    res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
