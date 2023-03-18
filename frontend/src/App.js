0436fa272e486883c20e4b54a059e1e1beb6d607
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-03-18 13:36:38 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,95 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Search from "./components/Search/Search";
-import Home from "./components/Home/Home";
-import Account from "./components/Account/Account";
-import Categories from "./components/Categories/Categories";
-import Cart from "./components/Cart/Cart";
-
+import { useEffect, useState } from "react";
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
+import ProductDetails from "./component/Product/ProductDetails";
+import Products from "./component/Product/Products";
+import Search from "./component/Product/Search";
+import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions";
+import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+import UpdateProfile from "./component/User/UpdateProfile";
+import UpdatePassword from "./component/User/UpdatePassword";
+import ForgotPassword from "./component/User/ForgotPassword";
+import ResetPassword from "./component/User/ResetPassword";
+import Cart from "./component/Cart/Cart";
+import Shipping from "./component/Cart/Shipping";
+import ConfirmOrder from "./component/Cart/ConfirmOrder";
+import axios from "axios";
+import Payment from "./component/Cart/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import OrderSuccess from "./component/Cart/OrderSuccess";
+import MyOrders from "./component/Order/MyOrders";
+import OrderDetails from "./component/Order/OrderDetails";
+import Dashboard from "./component/Admin/Dashboard.js";
+import ProductList from "./component/Admin/ProductList.js";
+import NewProduct from "./component/Admin/NewProduct";
+import UpdateProduct from "./component/Admin/UpdateProduct";
+import OrderList from "./component/Admin/OrderList";
+import ProcessOrder from "./component/Admin/ProcessOrder";
+import UsersList from "./component/Admin/UsersList";
+import UpdateUser from "./component/Admin/UpdateUser";
+import ProductReviews from "./component/Admin/ProductReviews";
+import Contact from "./component/layout/Contact/Contact";
+import About from "./component/layout/About/About";
+import NotFound from "./component/layout/Not Found/NotFound";
 function App() {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/v1/stripeapikey");
+
+    setStripeApiKey(data.stripeApiKey);
+  }
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+
+    store.dispatch(loadUser());
+
+    getStripeApiKey();
+  }, []);
+
+  window.addEventListener("contextmenu", (e) => e.preventDefault());
+
   return (
     <div className="App">
       <Router>
         <Header />
-        <Search />
+
+        {isAuthenticated && <UserOptions user={user} />}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/categories" element={<Categories />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
+          <Route exact path="/product/:id" element={<ProductDetails />} />
+          <Route exact path="/search" element={<Search />} />
+
+          <Route exact path="/contact" element={<Contact />} />
+          <Route element={<ProtectedRoute />}>
+            <Route element={<UpdatePassword />} exact path="/password/update" />
+            <Route element={<Shipping />} exact path="/shipping" />
+          </Route>
+            <Route element={<Profile />} exact path="/account" />
+            <Route element={<UpdateProfile />} exact path="/me/update" />
+
+          <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
