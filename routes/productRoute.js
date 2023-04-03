ea2f5e20b131e68ad846be4b50c58d13bdfd001d
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-04-03 12:49:44 +0300

    Update productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,37 +5,37 @@ const {
   updateProduct,
   deleteProduct,
   getProductDetails,
+  getAdminProducts,
   createProductReview,
   getProductReviews,
   deleteReview,
-  getAdminProducts,
-} = require("../controllers/productController");
+} = require("../controllers/productContoller");
+
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+router
+  .route("admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/product/:id")
+  .put(updateProduct)
+  .delete(deleteProduct)
+  .get(getProductDetails);
 
 router
   .route("/admin/products")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
+router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put( createProductReview);
+
 router
-  .route("/product/new")
-  .post(isAuthenticatedUser, authorizeRoles("admin"),  createProduct);
-
-// router
-//   .route("/admin/product/:id")
-//   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
-//   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
-
-// router.route("/product/:id").get(getProductDetails);
-
-// router.route("/review").put(isAuthenticatedUser, createProductReview);
-
-// router
-//   .route("/reviews")
-//   .get(getProductReviews)
-//   .delete(isAuthenticatedUser, deleteReview);
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
