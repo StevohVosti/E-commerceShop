b6052d1cd357a4a7e4ecdfd0bde5e92456673ead
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-04-03 12:48:44 +0300

    Update userAction.js

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -58,14 +58,20 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // Register
-export const register = (userData) => async (dispatch) => {
+export const register = (name, email, password, avatar) => async (dispatch) => {
   try {
     dispatch({ type: REGISTER_USER_REQUEST });
 
-    const config = { headers: { "Content-Type": "multipart/form-data" } };
-
-    const { data } = await axios.post(`/api/v1/register`, userData, config);
-
+    // const config = { headers: { "Content-Type": "multipart/form-data" } };
+    const { data } = await axios.post(
+      "/api/v1/register",
+      { name, email, password, avatar },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
     dispatch({
