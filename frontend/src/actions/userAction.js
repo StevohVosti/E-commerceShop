e8999ff9b5d5ffea8bba1308f3a10b1954fe8780
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-04-04 16:51:29 +0300

    Update userAction.js

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -58,28 +58,34 @@ export const login = (email, password) => async (dispatch) => {
 };
 
 // Register
-export const register = (name, email, password, avatar) => async (dispatch) => {
-  try {
-    dispatch({ type: REGISTER_USER_REQUEST });
+export const register=
+  (name, email, password, avatar) => async (dispatch) => {
+    try {
+      dispatch({
+        type: REGISTER_USER_REQUEST,
+      });
 
-    // const config = { headers: { "Content-Type": "multipart/form-data" } };
-    const { data } = await axios.post(
-      "/api/v1/register",
-      { name, email, password, avatar },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
-  } catch (error) {
-    dispatch({
-      type: REGISTER_USER_FAIL,
-      payload: error.response.data.message,
-    });
-  }
-};
+      const { data } = await axios.post(
+        "/api/v1/register",
+        { name, email, password, avatar },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: data.user,
+      });
+    } catch (error) {
+      dispatch({
+        type: REGISTER_USER_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
 
 // Load User
 export const loadUser = () => async (dispatch) => {
