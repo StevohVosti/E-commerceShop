d657f11d8a14c1726b1c3c79fd31d2dd8549009a
Stephen Maina <104502186+StevohVosti@users.noreply.github.com>
2023-04-11 19:45:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,15 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
 app.use(fileUpload());
 
 // Route Imports
